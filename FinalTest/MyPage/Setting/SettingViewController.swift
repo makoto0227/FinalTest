691e0bb3cdc544c7cdd6bf8b1626c7d6e71ece81
miyazaki makoto <miyazakimakoto@miyazakimakotonoMacBook-Air.local>
2019-12-12 16:28:02 +0900

    add mypage

---
+++ FinalTest/MyPage/Setting/SettingViewController.swift
@@ -0,0 +1,59 @@
+//
+//  SettingViewController.swift
+//  FinalTest
+//
+//  Created by 宮崎真 on 2019/12/12.
+//  Copyright © 2019 宮崎真. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+// MARK: - Property
+class SettingViewController: BaseViewController {
+    
+    @IBAction func touchedCancelButton(_ sender: UIButton) {
+        dismiss(animated: true) {}
+    }
+    @IBAction func touchedDoneButton(_ sender: UIButton) {
+        dismiss(animated: true) {}
+    }
+    @IBOutlet weak var userIcon: UIImageView!
+    @IBAction func touchedEditImageButton(_ sender: UIButton) {
+    }
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func touchedLogoutButton(_ sender: UIButton) {
+    }
+    @IBAction func touchedDeleteButton(_ sender: UIButton) {
+    }
+    
+}
+
+// MARK: - Life cycle
+extension SettingViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension SettingViewController {
+    
+}
+
+// MARK: - method
+extension SettingViewController {
+    
+}
